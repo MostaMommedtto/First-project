fe87e4104f1b372eeb82dbcdba518a7234a28b5c
Mostafa Mohamed <Mostamommedtto@gmail.com>
2021-09-09 11:32:49 +0200

    updated

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
-        final String filePath = "E:\\projects\\Java\\First project\\userInfo.csv";
         while (true) {
             System.out.println("Menu: ");
             System.out.println(" 1. Register \n 2. Display \n 3. Continue \n 4. Exit");
@@ -22,9 +21,9 @@ public class Main {
                 System.out.print("Enter your password: ");
                 String password = input.next();
                 User user = new User(firstName, lastName, userName, password);
-                User.writeData(user,filePath);
+                user.register(user);
             } else if (choice == 2) {
-                User.readData(filePath);
+                User.display();
             } else if (choice == 3) {
                 continue;
             } else if (choice == 4) {
--- src/MainPackage/User.java
+++ src/MainPackage/User.java
@@ -4,17 +4,14 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.UUID;
 
-public class User {
+public class User extends CSVFile{
 
     private final UUID uuid = UUID.randomUUID();
+    public static final String filePath = "E:\\projects\\Java\\First project\\userInfo.csv";
     private String firstName;
     private String lastName;
     private String userName;
     private String password;
-
-    public UUID getUuid() {
-        return uuid;
-    }
 
     public User(String firstName, String lastName, String userName, String password) {
         this.firstName = firstName;
@@ -22,6 +19,11 @@ public class User {
         this.userName = userName;
         this.password = password;
     }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
 
     public String getFirstName() {
         return firstName;
@@ -40,24 +42,14 @@ public class User {
     }
 
 
-    // public static void register(User client) {userInfo.add(client);}
-
-
-    public static void writeData(User client , String filePath) throws IOException {
-        FileWriter writer = new FileWriter(filePath,true);
-        writer.write(client.firstName + " " + client.lastName + " " + client.userName + " " + client.password);
-        writer.write("\r\n");
-        writer.flush();
-        writer.close();
+    public static void register(User client) throws IOException {
+        writeData(client, client.filePath);
     }
 
-    public static void readData(String filePath) throws FileNotFoundException {
-        Scanner input = new Scanner(new File(filePath));
-        input.useDelimiter(",");
-        while (input.hasNext()){
-            System.out.print(input.next() + " ");
-        }
-        input.close();
+    public static void display() throws FileNotFoundException {
+        readData(User.filePath);
     }
+
+
 
 }
